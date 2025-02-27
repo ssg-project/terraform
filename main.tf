ae43f0cfa6e6cd1da4cd72dae4578ca2b6c6b718
kuni4210 <kuni4210@naver.com>
2025-02-27 15:57:40 +0900

    init

---
+++ main.tf
@@ -0,0 +1,313 @@
+################################################################################
+# VPC
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${local.eks_name}-vpc"
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+    # Tags subnets for Karpenter auto-discovery
+    "karpenter.sh/discovery" = local.eks_name
+  }
+
+  tags = local.tags
+}
+
+################################################################################
+# Cluster
+################################################################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.24"
+
+  cluster_name    = local.eks_name
+  cluster_version = local.eks_version
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+  # cluster_addons = {
+  #   coredns = {
+  #     resolve_conflicts = "OVERWRITE"
+  #     configuration_values = jsonencode({
+  #       tolerations = [
+  #         # Allow CoreDNS to run on the same nodes as the Karpenter controller
+  #         # for use during cluster creation when Karpenter nodes do not yet exist
+  #         {
+  #           key    = "karpenter.sh/controller"
+  #           value  = "true"
+  #           effect = "NoSchedule"
+  #         }
+  #       ]
+  #     })
+  #   }
+  #   vpc-cni = {
+  #     resolve_conflicts = "OVERWRITE"
+  #     configuration_values = jsonencode({
+  #       enableNetworkPolicy = "true"
+  #       env = {
+  #         ENABLE_PREFIX_DELEGATION = "true"
+  #         WARM_ENI_TARGET          = "1"
+  #         MINIMUM_IP_TARGET        = "10"
+  #       }
+  #     })
+  #   }
+  #   aws-ebs-csi-driver = {
+  #     most_recent              = true
+  #     service_account_role_arn = aws_iam_role.ebs_csi.arn
+  #   }
+  #   metrics-server         = { resolve_conflicts = "OVERWRITE" }
+  #   eks-pod-identity-agent = {}
+  #   kube-proxy             = {}
+  # }
+
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    karpenter = {
+      ami_type = "BOTTLEROCKET_x86_64"
+      # instance_types = ["m5.large"]
+      instance_types = ["t3.medium"]
+
+      min_size     = local.eks_node_min_size
+      max_size     = local.eks_node_max_size
+      desired_size = local.eks_node_desired_size
+
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+
+      capacity_type = "SPOT"
+
+      labels = {
+        # Used to ensure Karpenter runs on nodes that it does not manage
+        "karpenter.sh/controller" = "true"
+      }
+
+      taints = {
+        # The pods that do not tolerate this taint should run on nodes
+        # created by Karpenter
+        karpenter = {
+          key    = "karpenter.sh/controller"
+          value  = "true"
+          effect = "NO_SCHEDULE"
+        }
+      }
+    }
+  }
+
+  node_security_group_tags = merge(local.tags, {
+    # NOTE - if creating multiple security groups with this module, only tag the
+    # security group that Karpenter should utilize with the following tag
+    # (i.e. - at most, only one security group should have this tag in your account)
+    "karpenter.sh/discovery" = local.eks_name
+  })
+
+  tags = local.tags
+}
+
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = "aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name}"
+}
+
+################################################################################
+# Blueprints Addon
+################################################################################
+
+module "eks_blueprints_addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.1"
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  eks_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+      configuration_values = jsonencode({
+        tolerations = [
+          # Allow CoreDNS to run on the same nodes as the Karpenter controller
+          # for use during cluster creation when Karpenter nodes do not yet exist
+          {
+            key    = "karpenter.sh/controller"
+            value  = "true"
+            effect = "NoSchedule"
+          }
+        ]
+      })
+    }
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+      configuration_values = jsonencode({
+        enableNetworkPolicy = "true"
+        env = {
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_ENI_TARGET          = "1"
+          MINIMUM_IP_TARGET        = "10"
+        }
+      })
+    }
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+      configuration_values = jsonencode({
+        controller = {
+          tolerations = [
+            {
+              key      = "karpenter.sh/controller"
+              operator = "Exists"
+              effect   = "NoSchedule"
+            },
+            {
+              key      = "CriticalAddonsOnly"
+              operator = "Exists"
+            }
+          ]
+        },
+        node = {
+          tolerations = [
+            {
+              key      = "karpenter.sh/controller"
+              operator = "Exists"
+              effect   = "NoSchedule"
+            },
+            {
+              key      = "CriticalAddonsOnly"
+              operator = "Exists"
+            }
+          ]
+        }
+    }) }
+    metrics-server = {
+      most_recent       = true
+      resolve_conflicts = "OVERWRITE" # 기존 설정 덮어쓰기
+      configuration_values = jsonencode({
+        replicas = 2,
+        tolerations = [
+          {
+            key      = "CriticalAddonsOnly",
+            operator = "Exists",
+            effect   = "NoSchedule"
+          },
+          {
+            key : "karpenter.sh/controller",
+            operator : "Exists",
+            effect : "NoSchedule"
+          }
+        ],
+        resources = {
+          requests = { cpu : "100m", memory : "128Mi" },
+          limits : { cpu : "200m", memory : "256Mi" }
+        },
+        # podDisruptionBudget : { enabled : true, maxUnavailable : 1 },
+        # nodeSelector : { kubernetes.io / os : "linux" },
+        # affinity : null # 특정 노드 배치를 위한 affinity 설정 가능
+      })
+    }
+  }
+
+  enable_aws_load_balancer_controller = true
+}
+
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.20"
+
+  role_name_prefix = "${local.eks_name}-ebs-csi-driver-"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = local.tags
+}
+
+################################################################################
+# Karpenter
+################################################################################
+
+module "karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "~> 20.24"
+
+  cluster_name          = module.eks.cluster_name
+  enable_v1_permissions = true
+  namespace             = local.karpenter_namespace
+
+  # Name needs to match role name passed to the EC2NodeClass
+  node_iam_role_use_name_prefix   = false
+  node_iam_role_name              = local.eks_name
+  create_pod_identity_association = true
+
+  tags = local.tags
+}
+
+################################################################################
+# Helm charts
+################################################################################
+
+resource "helm_release" "karpenter" {
+  name             = "karpenter"
+  namespace        = local.karpenter_namespace
+  create_namespace = true
+  repository       = "oci://public.ecr.aws/karpenter"
+  # repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  # repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart   = "karpenter"
+  version = "1.0.2"
+  wait    = false
+
+  values = [
+    <<-EOT
+    nodeSelector:
+      karpenter.sh/controller: 'true'
+    settings:
+      clusterName: ${module.eks.cluster_name}
+      clusterEndpoint: ${module.eks.cluster_endpoint}
+      interruptionQueue: ${module.karpenter.queue_name}
+    tolerations:
+      - key: CriticalAddonsOnly
+        operator: Exists
+      - key: karpenter.sh/controller
+        operator: Exists
+        effect: NoSchedule
+    webhook:
+      enabled: false
+    EOT
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      repository_password
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,33 @@
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.ecr
+}
+
+data "aws_availability_zones" "available" {
+  # Do not include local zones
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  # network
+  region   = "ap-northeast-2"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  vpc_cidr = "10.0.0.0/16"
+
+  # eks
+  eks_name              = "karpenter-cls"
+  eks_version           = "1.32"
+  eks_node_min_size     = 1
+  eks_node_max_size     = 3
+  eks_node_desired_size = 1
+
+  # karpenter
+  karpenter_namespace = "karpenter"
+
+  # tag
+  tags = {
+    Blueprint = local.eks_name
+  }
+}
